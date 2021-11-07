dc2fdb64c9ca56f14696dd283acf2639aee56e5e
ESafty <93883119+ESafty@users.noreply.github.com>
2021-11-07 22:31:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 void main() {
   int Y;
 
-  ANSELA.RA1 = 1;
+  ANSELA.RA1 = 1; // setting RA1 as analogue input and port D as digital output
   ANSELD=0;
   TRISA.RA1 = 1;
   TRISD=0;
   
-  UART1_Init(9600);
+  UART1_Init(9600); 
   Delay_ms(100);
   
-  UART_Write_Text("LDR Readings:");
+  UART_Write_Text("LDR Readings:");  // displays on the UART monitor the message "LDR Readings"
   UART1_Write(13);
   UART1_Write(10);
   Delay_ms(1000);
 
-  while(1)
+  while(1) // infinite loop
   {
-  Z = ADC_Read(1);
+  Z = ADC_Read(1); // reads the analogue value of the LDR from RA1
 
-  if(Z > 500)
+  if(Z > 500) // condition of resistance 
   {
    UART1_Write_Text("limuinous");
    UART1_Write(13);
    UART1_Write(10);
    Delay_ms(500);
-   PORTD = 0xFF;
+   PORTD = 0xFF;  // PORT D = Activate
    }
    else
    {
@@ -32,7 +32,7 @@ void main() {
    UART1_Write(13);
    UART1_Write(10);
    Delay_ms(100);
-   PORTD = 0x00;
+   PORTD = 0x00; // PORT D = Deactivate
    }
    
   }
